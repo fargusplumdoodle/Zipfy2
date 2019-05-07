3174393f8f10052368a7e1aabdd78ea9e70204b8
Isaac Thiessen <ike.thiessen0@gmail.com>
2019-05-07 15:57:15 -0700

    added GlobalStatistics component

--- ZipfyUI/app_server/components/GlobalStats.jsx
+++ ZipfyUI/app_server/components/GlobalStats.jsx
@@ -4,44 +4,55 @@ const DocInput = require('./DocInput');
 const ParetoChecker = require('./ParetoChecker');
 const Stats = require('./Stats');
 
-class Zipfy extends React.Component {
+
+class GlobalStats extends React.Component {
 	constructor(props){
 		super(props);
 		this.analyzeDocument = this.analyzeDocument.bind(this);
 		this.state = {
 		    words: [],
-            showResults: false
+            loading: true
 		};
+	}
+
+	handleHTTPErrors(response) {
+		if (!response.ok) throw Error(response.status + ': ' + response.statusText);
+		return response;
+
+	}
+
+	componentDidMount()	{
+	    // fetching globabal statistics
+		console.log('fetch started');
+		/*
+		fetch("http://localhost:8000/api/v1/", {
+			method: 'GET',
+		})
+			.then(response => this.handleHTTPErrors(response))
+			.then(response => response.json())
+			.then(response=> this.analyzeDocument(response))
+			.catch(error=>{ console.log("Fetch API error: " + error)});
+		*/
+		console.log('fetch done');
 	}
 
 	analyzeDocument(doc) {
 	    this.setState({
-			showResults: true,
+			loading: false,
 			words: doc.words, // finding each word that shows up once or more
-			wordCount: doc.wordCount,
-			paretoRatio: doc.paretoRatio
+			// wordCount: doc.wordCount,
+			// paretoRatio: doc.paretoRatio
 		});
     }
 
 	render() {
-		if (this.state.showResults) {
-			// displaying results
-			return (
-				<div className={'zipfy'}>
-					<h3 className={'hdr'}>Zipfs Law Global Statistics</h3>
-					<p>eeeeyy lmao</p>
-				</div>
-			)
-		} else {
-			// Ready for user input
-			return (
-				<div className={'zipfy'}>
-					<h3 className={'hdr'}>Zipfs Law Global Statistics</h3>
-                    <p>loading...</p>
-				</div>
-			)
-		}
+		return (
+			<div className={'zipfy'}>
+				<h3 className={'hdr'}>Zipfs Law Global Statistics</h3>
+				<p>loading...</p>
+			</div>
+		);
 	}
 }
 
-module.exports = Zipfy;
+module.exports = GlobalStats;
